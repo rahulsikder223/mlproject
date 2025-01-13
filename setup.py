bbdfb01e5caa31769cb5b98cdccacc4a5e6dde94
Bishwadeep Sikder <rahulsikder223@gmail.com>
2025-01-13 00:38:46 -0500

    project structure, logging, exception

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+def get_requirements(file_path):
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+        
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Bish',
+    author_email='rahulsikder223@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
